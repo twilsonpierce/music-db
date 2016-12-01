aa9754acb7a8ae136521ec2e48a7c03855116a27
Tionna Wilson-Pierce <tionna.wilsonpierce@gmail.com>
2016-12-01 15:05:46 -0500

    adding endpoints

--- server.js
+++ server.js
@@ -15,3 +15,40 @@ app.listen('9999', () => console.log('Listening on port 9999'));
 //////////
 // YOUR CODE HERE:
 //////////
+
+app.get('/api/artists', (req,res)=>{
+	Artist.findAll()
+	.then((data)=>{
+	console.log(data, 'We have all the artists!');
+	res.send(data);
+	})
+}).catch((error) =>{
+	console.log(error, 'Error!!!');
+})
+
+
+app.get('/api/artists/:id', (req,res)=>{
+	Artist.findById(req.params.id)
+	.then((data)=>{
+		console.log(data, 'We got this artists!')
+		res.send(data);
+	})
+}).catch((error)=>{
+	console.log(error,'Error!!!!')
+})
+
+app.post('/api/artists', (req,res) =>{
+	Artist.create({name:req.body.name}).then((data)=>{
+		console.log(data, 'New Artists!');
+		res.send(data);
+	})
+}).catch((error) =>{
+	console.log(error,'Error!!!')
+})
+
+app.put('/api/artists/:id/newName', (req,res)=>{
+	Artist.update({name:req.body.name},{where:{id:req.params.id}})
+	.then((data)=>{
+		console.log(data, 'Artist is update!!')
+	})
+})
