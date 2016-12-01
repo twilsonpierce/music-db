244f985505debd564535c8b1d0debe4235a9c2f1
Crazy Kiwi <Nasita-Haque>
2016-12-01 15:03:01 -0500

    adding models

--- models/genre-model.js
+++ models/genre-model.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const sequelizeConnection = require('../db');
-const Song = require('./song-model.js');
+
 
 //////////
 // YOUR CODE HERE:
@@ -15,7 +15,6 @@ var Genre = sequelizeConnection.define('genre', {
 	}
 });
 
-// Genre.belongsToMany(Song, {through: 'UniqueGenres'})
-//genre does not have song id.
+
 
 module.exports = Genre;
