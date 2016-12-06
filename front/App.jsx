e0f4d134af52234976e8b8f1953d3b74615027ab
Crazy Kiwi <Nasita-Haque>
2016-12-06 12:50:26 -0500

    changes made

--- front/App.jsx
+++ front/App.jsx
@@ -2,11 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, Link, browserHistory} from 'react-router';
-<<<<<<< HEAD
 // import './css-folder/Navbar.css';
-=======
-
->>>>>>> 308e8c48e77dd23426efef27e2610e30ebbb2d6b
 
 //Files
 import Navbar from './Component/Navbar';
@@ -30,12 +26,12 @@ const App = React.createClass({
 ReactDOM.render(
 <Router history={browserHistory}>
 	<Route path="/" component={App} >
-	<Route path="/artists" component={Artists}/>
-	<Route path="/songs" component={Songs}/>
-	<Route path="/playlist" component={Playlist}/>
-	<Route path="/create-playlist" component={CreatePlaylist}/>
-	<Route path="/individual-playlist" component={IndividualPlaylist}/>
-</Route>
+		<Route path="/artists" component={Artists}/>
+		<Route path="/songs" component={Songs}/>
+		<Route path="/playlist" component={Playlist}/>
+		<Route path="/create-playlist" component={CreatePlaylist}/>
+		<Route path="/individual-playlist" component={IndividualPlaylist}/>
+	</Route>
 </Router>
 ,document.getElementById('app')
 )
--- front/Component/Artists.js
+++ front/Component/Artists.js
@@ -1,31 +1,29 @@
 import React from 'react';
 import $ from 'jquery';
 
-const Artist = React.createClass({
-
-getInitialState(){
-	return ({artists: []})
-},
-ComponentDidMount(){
-let this = that;
-	$.ajax({
-		url: '/api/artists',
-		type:'GET',
-		success: ((data)=>{
-			console.log(data, 'this is the data!!!');
-			data ? return that.setState({artists:data}): console.log("error");
+const Artists = React.createClass({
+	getInitialState(){
+		return ({artists: []})
+	},
+	ComponentDidMount(){
+	// let this = that;
+		$.ajax({
+			url: '/api/artists',
+			type:'GET',
+			success: ((data)=>{
+				console.log(data, 'this is the data!!!');
+				data ? return this.setState({artists:data}): console.log("error");
+			})
 		})
-	})
-},
+	},
 	render(){
 		// let displayArtists = this.state.artists.map((value, index)=>{
 		// 	console.log()
 		// })
 		return (
-		<div>
-
-		</div>
-
+		<div></div>
 		)
 	}
 })
+
+export default Artists;
